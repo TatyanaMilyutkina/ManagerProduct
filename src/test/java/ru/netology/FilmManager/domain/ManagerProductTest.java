d1c0fb6e534724fb543e5feebb01b9e5881a3c9b
TatyanaMilyutkina <milytkinata@gmail.com>
2022-07-12 12:31:00 +0400

    NewTest

--- src/test/java/ru/netology/FilmManager/domain/ManagerProductTest.java
+++ src/test/java/ru/netology/FilmManager/domain/ManagerProductTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ManagerProductTest {
-    Book book1 = new Book(1, "Name1", 100, "Author1");
-    Book book2 = new Book(2, "Name2", 200, "Author2");
-    Book book3 = new Book(3, "Name3", 300, "Author3");
-    Smartphone smartphone1 = new Smartphone(4, "Name4", 400, "M1");
-    Smartphone smartphone2 = new Smartphone(5, "Name5", 500, "M2");
-    Smartphone smartphone3 = new Smartphone(6, "Name6", 600, "M3");
-    Smartphone smartphone4 = new Smartphone(7, "Name4", 700, "M4");
+    Book book1 = new Book(1, "NameBook1", 100, "Author1");
+    Book book2 = new Book(2, "NameBook2", 200, "Author2");
+    Book book3 = new Book(3, "NameBook3", 300, "Author3");
+    Smartphone smartphone1 = new Smartphone(4, "NameSma4", 400, "M1");
+    Smartphone smartphone2 = new Smartphone(5, "NameSma5", 500, "M2");
+    Smartphone smartphone3 = new Smartphone(6, "NameSma6", 600, "M3");
+    Smartphone smartphone4 = new Smartphone(7, "NameSma4", 700, "M4");
 
     @Test
     public void managerSaveAndFindAll() {
@@ -60,6 +60,17 @@ public class ManagerProductTest {
 
         Product[] actual = manager.searchByName("Name6");
         Product[] expected = {};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void managerSearchByMoreText() {
+        ManagerProduct manager = new ManagerProduct();
+        manager.save(book1);
+        manager.save(smartphone3);
+        manager.save(smartphone1);
+
+        Product[] actual = manager.searchByName("Sma");
+        Product[] expected = {smartphone3 , smartphone1};
         Assertions.assertArrayEquals(expected, actual);
     }
 
